429e027200d5d5803e517ffcaacfe97b94b84336
wsmolchanov <ws.molchanov@gmail.com>
2019-09-23 19:43:05 +0300

    add signin

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import classes from './Signup.module.css';
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
+// import thunk from "redux-thunk";
+import { signup } from '../../redux/action/auth.thunk';
+import { Field, reduxForm } from "redux-form";
 
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    color: red;
+  }
+`;
 
 
 export class Signup extends Component {
@@ -19,6 +29,8 @@ export class Signup extends Component {
 
     this.registerHandler.bind(this);
   }
+  
+    
 
   updateFormState = (params) => {
     this.setState(params);
@@ -31,38 +43,61 @@ export class Signup extends Component {
     const postData = { ...this.state };
     delete postData.confirmedPassword;
 
+    this.props.signup(this.state);
 
-    fetch('http://localhost:5000/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(postData)
-    }).then(res => {
-      console.log(res);
-    });
+    // itemsFetchData()
+    // fetch('http://localhost:5000/auth/register', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(postData)
+    // }).then(res => {
+    //   console.log(res);
+    // });
   }
 
+  
+
+  
+  
+
   render() {
-    console.log(this.props);
     return (
       <div className={classes.Signup}>
         <div>
+          <GlobalStyle />
+          <Link type="submit" to="/login">Login</Link>
           <h1>Registrer</h1>
 
           <form onSubmit={this.registerHandler} className={classes.SignupForm}>
             <label htmlFor="username">Username</label>
-            <input type="username" id="username" value={this.state.username} onChange={($event) => this.updateFormState({ ...this.state, username: $event.target.value })} />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email"  value={this.state.email} onChange={($event) => this.updateFormState({ ...this.state, email: $event.target.value })} />
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" value={this.state.password} onChange={($event) => this.updateFormState({ ...this.state, password: $event.target.value })} />
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input type="password" id="confirmPassword" value={this.state.confirmedPassword} onChange={($event) => this.updateFormState({ ...this.state, confirmedPassword: $event.target.value })}
+            <input 
+              type="username" id="username" 
+              value={this.state.username} 
+              onChange={($event) => this.updateFormState({ ...this.state, username: $event.target.value })} 
             />
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" id="email"  
+              value={this.state.email} 
+              onChange={($event) => this.updateFormState({ ...this.state, email: $event.target.value })} 
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" id="password" 
+              value={this.state.password} 
+              onChange={($event) => this.updateFormState({ ...this.state, password: $event.target.value })}
+            />
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input 
+              type="password" id="confirmPassword" 
+              value={this.state.confirmedPassword} 
+              onChange={($event) => this.updateFormState({ ...this.state, confirmedPassword: $event.target.value })}
+            />
+            
 
             <button type="submit">Register</button>
-            <Link to="/login">Login</Link>
 
           </form>
         </div>
@@ -77,6 +112,9 @@ const mapStateToProps = (state) => {
   }
 }
 
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,   
+  { signup }
 )(Signup);
+
