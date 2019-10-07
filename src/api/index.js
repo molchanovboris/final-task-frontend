0d1ef4e5f404803d1dd66178b56a928c4c1d6bd5
wsmolchanov <ws.molchanov@gmail.com>
2019-10-07 17:55:06 +0300

    add edituser getuser token

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,8 @@ let axiosInstance = axios.create({
   baseURL: 'http://localhost:5000',
 });
 
+axiosInstance.interceptors.request.use(const token = cookie.get(__TOKEN_KEY__););
+
+
+
 export default axiosInstance;
